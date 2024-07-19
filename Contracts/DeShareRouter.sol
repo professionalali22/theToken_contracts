3fefb9ff4528e219204f3bd107784d32b2be6683
professionalali22 <115666613+professionalali22@users.noreply.github.com>
2024-07-19 09:16:52 +0400

    Update DeShareRouter.sol

--- Contracts/DeShareRouter.sol
+++ Contracts/DeShareRouter.sol
@@ -11,7 +11,7 @@ import "./uniswap/v2-periphery/interfaces/IERC20.sol";
 import "./uniswap/v2-periphery/interfaces/IWETH.sol";
 import "./interfaces/ICommissionController.sol";
 
-contract UniswapV2Router02 is IUniswapV2Router02 {
+contract DeShareRouter is IUniswapV2Router02 {
     using SafeMath for uint;
 
     address public immutable override factory;
